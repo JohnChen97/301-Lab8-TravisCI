5213c07ba027a1e7608e363ab8e6db1617bbad4e
JohnChen97 <chenjionghao@yahoo.com>
2020-11-02 12:33:00 -0700

    Added a failed test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -28,4 +28,15 @@ class CityListTest {
         assertEquals(2, cityList.countCities());
         assertTrue(cityList.hasCity(new City("Regina", "Saskatchewan")));
     }
+
+    @Test
+    void testCount() {
+        CityList cityList = mockCityList();
+
+        assertEquals(0, cityList.countCities());
+
+
+    }
+
+
 }
