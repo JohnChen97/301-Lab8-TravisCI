2d49b78a7e0c78d39aa336f2319ae4883e474ce0
JohnChen97 <56984799+JohnChen97@users.noreply.github.com>
2020-11-02 15:17:02 -0700

    Update CityListTest.java

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -33,7 +33,7 @@ class CityListTest {
     void testCount() {
         CityList cityList = mockCityList();
 
-        assertEquals(0, cityList.countCities());
+        assertEquals(1, cityList.countCities());
 
 
     }
